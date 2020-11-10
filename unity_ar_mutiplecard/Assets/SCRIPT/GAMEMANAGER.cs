f9176c5bd4e95658e500582658e76227b9bfd14d
zxcc0209 <71679895+zxcc0209@users.noreply.github.com>
2020-11-10 12:00:51 +0800

    完成與發送

--- unity_ar_mutiplecard/Assets/SCRIPT/GAMEMANAGER.cs
+++ unity_ar_mutiplecard/Assets/SCRIPT/GAMEMANAGER.cs
@@ -14,6 +14,14 @@ public class GAMEMANAGER : MonoBehaviour
     public float turn = 1.5f;
     [Header("放大縮小"), Range(0,5f)]
     public float bigsmall = 0.1f;
+    [Header("兔子動畫元件")]
+    public Animator aniRabbit;
+    [Header("蝙蝠動畫元件")]
+    public Animator aniBat;
+
+    public float test = 1;
+    public float test2 = 1;
+
     //事件:(藍色)-初始設定
     private void Start()
     {
@@ -26,5 +34,44 @@ public class GAMEMANAGER : MonoBehaviour
         bat.localScale += new Vector3(1, 1, 1) * Joystick.Vertical*bigsmall;
         rabbit.Rotate(0, Joystick.Horizontal*turn, 0);
         rabbit.localScale += new Vector3(1, 1, 1) * Joystick.Vertical*bigsmall;
+
+        bat.localScale = new Vector3(1, 1, 1) * Mathf.Clamp(bat.localScale.x, 0.5f, 40f);
+        rabbit.localScale = new Vector3(1, 1, 1) * Mathf.Clamp(bat.localScale.x, 0.5f, 40f);
+
+
+        test = Mathf.Clamp(test, 0.1f, 10f);
+        print(Mathf.Clamp(test2, 0.1f, 10f))
+            ;
     }
+    //方法   修飾詞 類型  名稱(){}
+    public void Attack() 
+    {
+        print("攻擊");
+        aniRabbit.SetTrigger("兔子攻擊");
+        aniBat.SetTrigger("攻擊觸發");
+
+    }
+    public void DIE()
+    {
+        print("死亡");
+        aniRabbit.SetTrigger("兔子死亡");
+        aniBat.SetTrigger("死亡觸發");
+
+    }
+    public void MOVE()
+    {
+        print("移動");
+        aniRabbit.SetTrigger("兔子移動");
+        aniBat.SetTrigger("移動觸發");
+
+    }
+    //public void Attack(參數)
+    //參數  類型 名稱
+    //public void PlayAnimation(string aniName) 
+    //{
+    //  aniRabbit.SetTrigger(aniName);
+    //  aniBat.SetTrigger(aniName);
+    //然後到按鈕的click設定  打各自的攻擊名稱，由於我名稱不一樣不適合
+    //} 
+
 }
